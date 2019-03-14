6e9a3e5d49fe740176297b703252f83aee72c229
mikeygbd <mikey.soares@gmail.com>
2019-03-14 18:14:38 -0400

    added the wishlist parts table with all the same columns as parts

---
+++ van-builder/db/migrate/20190313001019_add_wishlist_parts_table.rb
@@ -0,0 +1,14 @@
+class AddWishlistPartsTable < ActiveRecord::Migration
+  def change
+    create_table :wishlist_parts do |t|
+      t.string  :name
+      t.integer :price
+      t.string  :description
+      t.string  :manufacturer
+      t.integer :user_id
+      t.string  :url
+      t.string  :page_link
+      t.integer :qty
+    end
+  end
+end
