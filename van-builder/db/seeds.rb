79c60aab5cc4356d2e3419fe79c5467b1ec61ff1
mikeygbd <mikey.soares@gmail.com>
2019-03-15 20:16:41 -0400

    created seed.rb file in db

---
+++ van-builder/db/seeds.rb
@@ -0,0 +1,12 @@
+# User.create(name: 'Joe Shmo', username: 'joeshmo@google.com', password: 'joe', van_make: 'Mercedes', van_model: 'Sprinter', van_year: 2014, van_wheelbase: 144, van_color: 'White')
+
+Part.create(manufacturer: 'Renogy', name: '160 Watt Solar Pannel', price: 299.99, qty: 4, description: 'flexible')
+Part.create(manufacturer: 'Renogy', name: 'Rover Li 40 Amp MPPT Solar Charge Controller', price: 199.99, qty: 1, description: 'Bluetooth')
+Part.create(manufacturer: 'Renogy', name: '2000W Pure Sine Wave Inverter Charger', price: 599.99, qty: 1, description: '')
+Part.create(manufacturer: 'Renogy', name: 'Renogy Deep Cycle AGM Battery 12 Volt 200Ah', price: 399.99, qty: 3, description: 'Renogys 12V deep cycle AGM batteries were specifically designed for solar energy storage applications. Leak-proof, spill-proof and ultimately maintenance free, Renogys AGM line is the ideal candidate for a battery meeting high quality performance. Whether its for daily use or backup power, the Renogy AGM battery is right for you.')
+Part.create(manufacturer: 'Renogy', name: 'Battery to Charge Controller Tray Cables for 3/8 in Lugs', price: 79.99, qty: 1, description: '16FT 4AWG')
+
+
+WishlistPart.create(manufacturer: '', name: 'Sprinter Windows 170" and 144" Passenger Side Sliding Door T-Vent- FW625R', price: 499.95, qty: 1, description: 'Venting Window')
+WishlistPart.create(manufacturer: '', name: 'SPRINTER WINDOWS 170"/144" DRIVERS SIDE FORWARD T-VENT- FW621L', price: 549.95, qty: 1, description: 'Venting Window')
+WishlistPart.create(manufacturer: 'Blue Sea', name: '5031 ST BLADE FUSE BLOCK W/O COVER', price: 499.99, qty: 1, description: '12 CIRCUIT W/NEGATIVE BUS')
